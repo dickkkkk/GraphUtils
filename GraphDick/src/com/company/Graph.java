f326e8759dfad5534d213f5df50bec983c74d488
dickkkkk <105591633+dickkkkk@users.noreply.github.com>
2022-10-12 11:41:00 +0300

    Add files via upload

---
+++ GraphDick/src/com/company/Graph.java
@@ -0,0 +1,110 @@
+package com.company;
+
+import java.util.*;
+
+public class Graph {
+
+    private final int maxVershin = getRandomNumber(8, 12);
+    private int[][] arr = new int[maxVershin][maxVershin];
+    private final String vidGraph = getRandomVid();
+
+    private int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    private String getRandomVid() {
+        String[] str = new String[]{"orGraph", "Graph"};
+        return str[getRandomNumber(0, 2)];
+    }
+
+    public void addEdge(int first, int second) {
+        if (vidGraph == "orGraph") {
+            arr[first][second] = 1;
+        } else {
+            arr[first][second] = 1;
+            arr[second][first] = 1;
+        }
+    }
+
+    public void printGraph() {
+        if (vidGraph == "orGraph") {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == 1) {
+                        System.out.println(i + "-->" + j);
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == 1) {
+                        System.out.println(i + "--" + j);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void createRandomGraph() {
+        for (int i = 0; i < maxVershin; i++) {
+            addEdge(getRandomNumber(0, maxVershin), getRandomNumber(0, maxVershin));
+        }
+    }
+
+    public List<Integer> dfs(int i0) {
+        List<Integer> dfs = new ArrayList<>();
+        boolean[] visited = new boolean[maxVershin];
+        visited[i0] = true;
+        dfs.add(i0);
+        class dfss {
+            Integer dfsss(int j) {
+                int v = 0;
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[j][i] == 1) {
+                        if (visited[i] != true) {
+                            visited[i] = true;
+                            v = i;
+                            dfs.add(v);
+                            dfsss(i);
+                        }
+                    }
+                }
+                return v;
+            }
+        }
+        new dfss().dfsss(i0);
+        return dfs;
+    }
+
+    public List<Integer> bfs(int i0) {
+        Queue<Integer> bfsQueue = new ArrayDeque<>();
+        List<Integer> bfs = new ArrayList<>();
+        boolean[] visited = new boolean[maxVershin];
+        bfs.add(i0);
+
+        class bfss {
+            void bfsss(int j) {
+                for (int i = 0; i < arr.length; i++) {
+                    if (visited[j] == false) {
+                        visited[j] = true;
+                    }
+                    if (arr[j][i] == 1 && visited[i] == false) {
+                        bfsQueue.add(i);
+                    }
+                }
+                if (!bfsQueue.isEmpty()) {
+                    Integer v = bfsQueue.remove();
+                    bfs.add(v);
+                    bfsss(v);
+                }
+            }
+        }
+        new bfss().bfsss(i0);
+        return bfs;
+    }
+}
+
+// Графы - насилие над животными :(
---
+++ GraphDick/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph gr = new Graph();
+
+        gr.createRandomGraph();
+        /*gr.addEdge(1, 2);
+        gr.addEdge(1, 3);
+        gr.addEdge(2, 3);
+        gr.addEdge(2, 4);
+        gr.addEdge(2, 4);
+        gr.addEdge(3, 4);
+        */
+        gr.printGraph();
+        System.out.println();
+        System.out.println("Введите начальную вершину для обходов: ");
+        int i0 = scn().nextInt();
+
+        System.out.println("Обход dfs: ");
+        List<Integer> l = gr.dfs(i0);
+        Iterator itr = l.iterator();
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+
+        System.out.println();
+        System.out.println("Обход bfs: ");
+        List<Integer> l2 = gr.bfs(i0);
+        Iterator it = l2.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+
+    public static Scanner scn(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+}
